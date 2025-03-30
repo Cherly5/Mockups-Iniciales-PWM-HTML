0f2b3ea09c25b42d8b4250ef6f162281451353da
santiagogape <santiago.galindo101@alu.ulpgc.es>
2025-03-30 18:48:39 +0100

    log-out

--- navigation-bar/navbar-reactive.js
+++ navigation-bar/navbar-reactive.js
@@ -12,14 +12,22 @@ function buttons(data, excludeIds, buttonContainer) {
         if (!excludeIds.includes(button.id)) {
             const btn = document.createElement("button");
             btn.className = button.id === "link-profile" ? "button-animated button-background-filled-reverse" : "button-animated button-background-filled";
-            const a = document.createElement("a");
-
-            a.href = project() + button.href;
-            a.id = button.id;
+            let a;
+            if (button.id !== "link-log-out") {
+                a = document.createElement("a");
+                a.href = project() + button.href;
+            } else {
+                a = document.createElement("span")
+                btn.addEventListener("click", function (_) {
+                    const currentUser = JSON.stringify(null)
+                    localStorage.setItem("currentUser", currentUser);
+                    window.location.href = "/" + window.location.pathname.split('/').at(1) + "/index/index.html";
+                });
+            }
             a.className = "button-link";
             a.textContent = button.label;
             btn.appendChild(a);
-
+            btn.classList.add(button.id)
             buttonContainer.appendChild(btn);
         }
     });
@@ -59,7 +67,6 @@ function updateMobileNavbar(data, excludeIds){
         clone.querySelector("span").innerText = link.title
         clone.querySelector("span").nextElementSibling.innerText = link.description
         links.appendChild(clone);
-
     }
     buttons(data, excludeIds, links.nextElementSibling);
 }
@@ -67,20 +74,19 @@ function updateMobileNavbar(data, excludeIds){
 function loadNavbar() {
     fetch("/navigation-bar/navbar.json") // Reemplaza con la ruta real del JSON
         .then(response => response.json())
-        .then(data => {
+        .then(async data => {
             const currentUser = JSON.parse(localStorage.getItem("currentUser"));
             if (currentUser === null) {
-                loadNavbarForGuestUser(data);
+                await loadNavbarForGuestUser(data);
             } else {
-                loadNavbarForAuthenticatedUser(data);
+                await loadNavbarForAuthenticatedUser(data);
             }
         })
         .catch(error => console.error("Error loading navbar JSON:", error));
 }
 
 // Llamar al método al cargar la página
-
-loadNavbar();
+loadNavbar()
 
 const burgerMenu = document.querySelector(".navbar-burger-menu");
 const mobileMenu = document.querySelector(".navbar-mobile-menu");
@@ -95,9 +101,4 @@ closeMenu.addEventListener("click", function () {
     burgerMenu.classList.remove("hidden");
 });
 
-document.getElementById("link-log-out").addEventListener("click", function () {
-    event.preventDefault();
-    const currentUser = JSON.stringify(null)
-    localStorage.setItem("currentUser", currentUser);
-    window.location.href = "/index/index.html";
-});
+
